cf8f76387583a8fb84bc96229d6741c278ef70bf
Luke Waller <lukerwaller@gmail.com>
2017-10-25 16:03:16 +0100

    deplotment commit ver9

--- s3project/settings.py
+++ s3project/settings.py
@@ -19,6 +19,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'y4l_$-izj8q44#6fu5rhzppsk=hyoy8ygmo1-(q-$zb0uj)4*c'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -125,8 +128,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
- 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
@@ -138,7 +139,9 @@ AUTHENTICATION_BACKENDS = (
     'accounts.backends.EmailAuth',
 )
 
+DISQUS_API_KEY = 'yZD9hxa2uNkjU6GhyqqoHe1lCulUlsPvQG2YiU0LEXMyV4df1RYj0ppSHEvqYXwF'
 DISQUS_WEBSITE_SHORTNAME = 'forthecrownblog'
+SITE_ID = 1
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
